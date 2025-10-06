e6862d24803b9ab2c95a7acaab96d52c6fa21176
sakiasi <saki.baleiwai.komai@gmail.com>
2025-10-06 19:55:54 +0000

    embadd form and list into the estate form and make list editable for quick edits

--- models/estate_property_offer.py
+++ models/estate_property_offer.py
@@ -3,6 +3,7 @@ from odoo import models,fields
 class EstateOffer(models.Model):
     _name = 'estate_offer'
     _description = 'Estate Offer'
+    _rec_name = 'buyer_id'
 
     price = fields.Float(string='Price')
     status = fields.Selection(
@@ -11,8 +12,10 @@ class EstateOffer(models.Model):
             ('refused','Refused'),
         ]
     )
-    partner_id = fields.Many2one('res.partner', string='Person', required=True)
     property_id = fields.Many2one('estate.model', string='Property', required=True)
+    
+    buyer_id = fields.Many2one('estate_buyer', string='Buyer')
 
     validity = fields.Integer(string='Validity', default=7)
+    
     date_deadline = fields.Date(string='Deadline')
